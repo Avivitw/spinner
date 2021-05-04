f91c2dc085053e187967f4c604842be59f3cfbdf
Avivit Weissman <wavivit@gmail.com>
2021-05-04 20:59:21 +0000

    refactor the code added more spinns and a new line

--- spinner1.js
+++ spinner1.js
@@ -13,9 +13,28 @@ setTimeout(() => {
 }, 500);
 
 setTimeout(() =>{
-  process.stdout.write(`\r\\    `);
+  process.stdout.write(`\r\\     `);
 }, 700);
 
 setTimeout(() => {
   process.stdout.write(`\r|     `);
 }, 1000);
+
+setTimeout(() => {
+  process.stdout.write(`\r/     `);
+},1200);
+
+setTimeout(() => {
+  process.stdout.write(`\r-     `);
+}, 1500);
+
+setTimeout(() =>{
+  process.stdout.write(`\r\\     `);
+}, 1800);
+
+setTimeout(() => {
+  process.stdout.write(`\r|     `);
+}, 2000);
+
+setTimeout(() => process.stdout.write(`\n`)
+, 2300);   
